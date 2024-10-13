83b3656370ee2a3db64594bf9c6476ece174e1f0
Maksim <fanmaxon@yandex.ru>
2024-10-13 12:30:49 +0300

    Добавление Л.Р №3

--- WebMVCR1/Controllers/HomeController.cs
+++ WebMVCR1/Controllers/HomeController.cs
@@ -1,17 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebMVCR1.Models;
 
+
 namespace WebMVCR1.Controllers
 {
     public class HomeController : Controller
     {
-        public string  Index(string hel)
-        {
-            string Greeting = ModelClass.ModelHello() + ", " + hel;
-            return Greeting;
+        //public string  Index(string hel)
+        //{
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel;
+        //    return Greeting;
 
+        //}
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+
+
+
+            return View();
         }
 
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
 
-    }
+       
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+			db.AddResponse(p);
+			return View("Hello", p);
+
+
+        }
+		private static PersonRepository db = new PersonRepository();
+
+		public ViewResult OutputData()
+		{
+			ViewBag.Pers = db.GetAllResponses;
+			ViewBag.Count = db.NumberOfPerson;
+			return View("ListPerson");
+		}
+
+
+	}
 }
--- WebMVCR1/Models/ModelClass.cs
+++ WebMVCR1/Models/ModelClass.cs
@@ -5,10 +5,22 @@
         public static string ModelHello()
         {
             int hour = DateTime.Now.Hour;
-            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
-   return Greeting;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; return Greeting;
         }
 
 
     }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() => FirstName + " " +
+       LastName;
+    }
+
+
+
+
+
+
 }
